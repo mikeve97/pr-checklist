4c80b1ec5e0fa812da79902b4de323da4d350d22
Mike van Es <Mike.vanEs@shell.com>
2023-11-13 15:09:26 +0100

    Update script

--- index.js
+++ index.js
@@ -8,36 +8,39 @@ try {
         return;
     }
 
-    const prBranchName = github.context.payload.pull_request.head.ref;
-    const prTitle = github.context.payload.pull_request.title;
-    const prBody = github.context.payload.pull_request.body;
-    const workItem = prBranchName.split('-')[0].trim();
+    const { ref: prBranchName = '', title: prTitle = '', body: prBody = '' } = github.context.payload.pull_request;
+    const workItemNumber = prBranchName?.match(/^(\d+)-/)?.[1];
 
     //log all the things
     core.info(`PR branch name: ${prBranchName}`);
     core.info(`PR title: ${prTitle}`);
     core.info(`PR body: ${prBody}`);
-    core.info(`Work item: ${workItem}`);
+    core.info(`Work item: ${workItemNumber}`);
 
-    if (isNaN(workItem)) {
+    if (isNaN(Number(workItemNumber))) {
         core.setFailed('Branch name does not include work item number.');
         return;
     }
 
-    if (prTitle?.includes(workItem) && prBody?.includes(workItem)) {
-        core.info('PR title and body already contains work item number.');
+    if (prTitle.includes(workItemNumber) && prBody.includes(workItemNumber)) {
+        core.info('PR title and body already contain the work item number.');
         return;
     }
 
-    const newPrTitle = `${workItem} - ${prTitle}`;
-    const newPrBody = `${prBody}\n Work item: AB#${workItem}`;
+    const newPrTitle = !prTitle.includes(workItemNumber) ?
+        `${workItemNumber} - ${prTitle || ''}` :
+        prTitle;
 
-    if (!prTitle.includes(workItem)) {
+    const newPrBody = !prBody.includes(workItemNumber) ?
+        `${prBody || ''}\n Work item: AB#${workItemNumber}` :
+        prBody;
+
+    if (newPrTitle !== prTitle) {
         core.info(`Updating PR title to: ${newPrTitle}`);
         core.setOutput('pr_title', newPrTitle);
     }
 
-    if (!prBody.includes(workItem)) {
+    if (newPrBody !== prBody) {
         core.info(`Updating PR body to: ${newPrBody}`);
         core.setOutput('pr_body', newPrBody);
     }
